a3c655f8d1ade8a39a2bb6e4a7f3da8959598e66
no <nope>
2019-11-01 17:51:36 +0100

    a

--- crates/siko_type_checker2/src/typechecker.rs
+++ crates/siko_type_checker2/src/typechecker.rs
@@ -38,10 +38,10 @@ impl InstanceInfo {
         }
     }
 
-    pub fn get_location(&self) -> &LocationId {
+    pub fn get_location(&self) -> LocationId {
         match self {
-            InstanceInfo::UserDefined(_, _, id) => &id,
-            InstanceInfo::AutoDerived(_, id) => &id,
+            InstanceInfo::UserDefined(_, _, id) => *id,
+            InstanceInfo::AutoDerived(_, id) => *id,
         }
     }
 }
@@ -125,20 +125,39 @@ impl InstanceResolver {
     pub fn check_conflicts(&self, errors: &mut Vec<TypecheckError>, program: &Program) {
         for (class_id, class_instances) in &self.instance_map {
             let class = program.classes.get(&class_id);
-            for (base_type, instances) in class_instances {
-                for (first_index, first_instance) in instances.iter().enumerate() {
-                    for (second_index, second_instance) in instances.iter().enumerate() {
-                        if first_index < second_index {
-                            let first = first_instance.get_type();
-                            let second = second_instance.get_type();
-                            let mut unifier = Unifier::new();
-                            if unifier.unify(first, second).is_ok() {
-                                let err = TypecheckError::ConflictingInstances(
-                                    class.name.clone(),
-                                    *first_instance.get_location(),
-                                    *second_instance.get_location(),
-                                );
-                                errors.push(err);
+            let mut first_generic_instance_location = None;
+            if let Some(generic_instances) = class_instances.get(&BaseType::Generic) {
+                first_generic_instance_location = Some(generic_instances[0].get_location());
+            }
+            for (_, instances) in class_instances {
+                if let Some(generic_location) = first_generic_instance_location {
+                    for instance in instances {
+                        let other_instance_location = instance.get_location();
+                        if other_instance_location == generic_location {
+                            continue;
+                        }
+                        let err = TypecheckError::ConflictingInstances(
+                            class.name.clone(),
+                            generic_location,
+                            other_instance_location,
+                        );
+                        errors.push(err);
+                    }
+                } else {
+                    for (first_index, first_instance) in instances.iter().enumerate() {
+                        for (second_index, second_instance) in instances.iter().enumerate() {
+                            if first_index < second_index {
+                                let first = first_instance.get_type();
+                                let second = second_instance.get_type();
+                                let mut unifier = Unifier::new();
+                                if unifier.unify(first, second).is_ok() {
+                                    let err = TypecheckError::ConflictingInstances(
+                                        class.name.clone(),
+                                        first_instance.get_location(),
+                                        second_instance.get_location(),
+                                    );
+                                    errors.push(err);
+                                }
                             }
                         }
                     }
